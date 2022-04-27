573681ae213a7c22f1116243949a5bcaea5e401d
ylin20220913 <102158345+ylin20220913@users.noreply.github.com>
2022-04-27 17:20:25 -0400

    Updated config.yml

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ result = subprocess.run(["lsusb"], stdout=subprocess.PIPE)
 result.stdout
 print("result is")
 print(result)
-import usb
-busses = usb.busses()
-for bus in busses:
-    devices = bus.devices
-    for dev in devices:
-        print("Device:", dev.filename)
-        print("  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor))
-        print("  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct))
+# import usb
+# busses = usb.busses()
+# for bus in busses:
+#     devices = bus.devices
+#     for dev in devices:
+#         print("Device:", dev.filename)
+#         print("  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor))
+#         print("  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct))
